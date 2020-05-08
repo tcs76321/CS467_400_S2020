6311265f5235b78db84b43630a82c0104cf3da1c
Trevor Stahl <trevor@solminion.com>
2020-05-07 18:31:29 -0700

    can create users and login, need to adress 3xtodo so that when authed go to secure auth-user based user profile page

--- src/Controller/IndexPageController.php
+++ src/Controller/IndexPageController.php
@@ -8,11 +8,21 @@ use Symfony\Component\Routing\Annotation\Route;
 class IndexPageController extends AbstractController
 {
     /**
-     * @Route("/")
+     * @Route("/", name="index_page")
      */
     public function index()
     {
         return $this->render('index_page/index.html.twig', [
+            'controller_name' => 'IndexPageController',
+        ]);
+    }
+
+    /**
+     * @Route("/success", name="createAcct_success_page")
+     */
+    public function success()
+    {
+        return $this->render('index_page/success.html.twig', [
             'controller_name' => 'IndexPageController',
         ]);
     }
---
+++ src/Migrations/Version20200508010058.php
@@ -0,0 +1,36 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20200508010058 extends AbstractMigration
+{
+    public function getDescription() : string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('CREATE TABLE user (id INT AUTO_INCREMENT NOT NULL, tribe_id INT DEFAULT NULL, email VARCHAR(180) NOT NULL, roles JSON NOT NULL, password VARCHAR(255) NOT NULL, UNIQUE INDEX UNIQ_8D93D649E7927C74 (email), UNIQUE INDEX UNIQ_8D93D6496F3EE0AD (tribe_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('ALTER TABLE user ADD CONSTRAINT FK_8D93D6496F3EE0AD FOREIGN KEY (tribe_id) REFERENCES tribe (id)');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('DROP TABLE user');
+    }
+}
