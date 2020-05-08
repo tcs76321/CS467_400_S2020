2238b091e41771643cd4968547d54d0514f625bf
Trevor Stahl <trevor@solminion.com>
2020-05-07 17:07:54 -0700

    add data fixtures and user, start of security and login

---
+++ src/Migrations/Version20200507192734.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20200507192734 extends AbstractMigration
+{
+    public function getDescription() : string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('CREATE TABLE tribe (id INT AUTO_INCREMENT NOT NULL, babies SMALLINT NOT NULL, children SMALLINT NOT NULL, adults SMALLINT NOT NULL, elders SMALLINT NOT NULL, horses SMALLINT NOT NULL, dogs SMALLINT NOT NULL, focus LONGTEXT DEFAULT NULL, direction LONGTEXT DEFAULT NULL, day SMALLINT NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('DROP TABLE tribe');
+    }
+}
